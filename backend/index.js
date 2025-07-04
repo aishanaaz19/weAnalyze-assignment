bf6dd32b88cc35408dc361844123a335dfb6309e
ayeshakarim19 <84725003+ayeshakarim19@users.noreply.github.com>
2025-07-04 05:33:26 +0530

    Updated routes

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,10 @@ import verifyToken from "./middleware/auth.middleware.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://we-analyze-assignment-go7b.vercel.app/",
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
